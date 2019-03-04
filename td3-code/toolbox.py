ffbb3a576ac8981e7485a70d09489f770f71c736
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2019-03-04 20:42:46 +0100

    passwords with 6 characters

--- td3-code/toolbox.py
+++ td3-code/toolbox.py
@@ -35,7 +35,7 @@ def getPassDict(nbpasswords):
     return passwords
 
 def genRandomPassword():
-    length = 12
+    length = 6
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for i in range(length))
 
